38661cf34e1f5ece2417d8832254a0fcb35e0fe5
jason <xzjhsy@gmail.com>
2024-10-21 09:36:24 +0800

    feat: 跳过https证书验证

--- src/tools/http_request.rs
+++ src/tools/http_request.rs
@@ -1,6 +1,10 @@
+use reqwest::Client;
 use serde::de::DeserializeOwned;
 
 pub async fn get<T: DeserializeOwned>(url: &str) -> Result<T, reqwest::Error> {
-    let res = reqwest::get(url).await?.json::<T>().await?;
+    let client = Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()?;
+    let res = client.get(url).send().await?.json::<T>().await?;
     Ok(res)
 }
